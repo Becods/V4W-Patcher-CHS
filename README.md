c07d7bb49e9a450ddc47a345288c0fb3e4d9a942
Becods <i@muir.fun>
2022-07-28 22:41:17 +0800

    Add Windows 11 Support

--- README.md
+++ README.md
@@ -1,32 +1,35 @@
 # ViPER4Windows-Patcher
-一个能让你在Windows10上完美使用ViPER4Windows的补丁
+一个能让你在Windows上完美使用ViPER4Windows的补丁
 
-><b>需要:</b> Windows-10 + PowerShell
+><b>支持:</b> Windows 10 / Windows 11
 
 作者 [metaspook](https://github.com/metaspook) 汉化 [BecodReyes](https://github.com/BecodReyes)
-
-### Screenshot:
-![Screenshot](https://p.pstatp.com/origin/138ac000087c5b60d4f4a)
 
 ### Features:
 * 修复不兼容问题
 * 修复注册表项问题
-* 可移植脚本，可在任何地方运行
+* 纯绿色脚本，可在任何地方运行
 * 管理权限请求
+* 自动切换修复方法
 
-### Download:
-#### [V4W-Patcher-CHS-master.zip](https://github.com/BecodReyes/V4W-Patcher-CHS/archive/master.zip)
+### Screenshot:
+![Screenshot1](screenshot1.png?raw=true)
+![Screenshot2](screenshot2.png?raw=true)
+
+
+><b>注意:</b> psexec.exe 是 Microsoft Sysinternals 的一部分，如果缺失可能会导致修复失败
 
 ### Instructions:
-* 确保正常安装了 ViPER4Windows
-* 下载 V4W-Patcher-CHS-master.zip
-* 将 V4W-Patcher-CHS.bat从V4W-Patcher-CHS-master.zip解压出来
-* 运行V4W-Patcher-CHS.bat，按照提示进行 😉
+* 确保 `ViPER4Windows` 已安装
+* 下载 [V4W-Patcher-CHS-master.zip](https://github.com/BecodReyes/V4W-Patcher-CHS/archive/master.zip)
+* 解压 `V4W-Patcher-CHS-master.zip` 中的所有文件
+* 运行 `V4W-Patcher-CHS.bat` ，按照提示进行 😉
 
 ### Change Log:
 <details><summary>单击折叠/展开</summary><br/>
 
 ```
+* v2.1 - 添加 Windows 11 支持.
 * v2.0 - 修复一些BUG和更新了UI
        - 修复了音频服务名称
        - 添加了"重启音频服务"
--- V4W-Patcher-CHS.bat
+++ V4W-Patcher-CHS.bat
@@ -1,8 +1,8 @@
 :: ViPER4Windows Patcher.
-:: Version: 2.0
+:: Version: 2.1
 :: Written by Metaspook
 :: License: <http://opensource.org/licenses/MIT>
-:: Copyright (c) 2019-2020 Metaspook.
+:: Copyright (c) 2019-2022 Metaspook.
 :: 
 @echo off
 
@@ -22,10 +22,12 @@ if '%errorlevel%' NEQ '0' (
 title "ViPER4Windows 修复程序"
 color 0B
 pushd "%~dp0"
-set APPVAR=2.0
+set APPVAR=2.1
 for /f "tokens=2*" %%X in ('REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\ViPER4Windows" /v ConfigPath') do set PAPPDIR=%%Y
 set APPDIR=%PAPPDIR:\DriverComm=%
-mode con: cols=91 lines=24
+set APOSubPath="AudioEngine\AudioProcessingObjects\{DA2FB532-3014-4B93-AD05-21B2C620F9C2}"
+set AppCompatFlagsLayers="SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers"
+mode con: cols=56 lines=20
 
 :CHOICE_MENU
 call:BANNER
@@ -39,19 +41,62 @@ echo      +--------------------+
 echo.
 echo 使用：选择一个数字，然后按回车键
 echo       建议在重启音频服务之前关闭音频输出，以免出现问题
-echo       不需要重启电脑
+echo       按照顺序执行，不需要重启电脑
 echo.
 set CMVAR=
 set /p "CMVAR=请选择: "
 if "%CMVAR%"=="0" exit
-if not exist "%APPDIR%" (
+if exist "%AppDir%\ViPER4WindowsCtrlPanel.exe" (
+	if "%CMVAR%"=="1" call:PATCH_REGISTRY
+	if "%CMVAR%"=="2" call:LAUNCH_CONFIGURATOR
+	if "%CMVAR%"=="3" (
+		call:BANNER
+		call:RESTART_AUDIO_SERVICE
+	)
+) else (
 	call:BANNER
-	echo [失败!] ViPER4Windows 未安装！
+	echo [失败!] ViPER4Windows 未安装.
 	>nul 2>&1 timeout /t 2
 	goto:CHOICE_MENU
 )
-if "%CMVAR%"=="1" (
+
+:PATCH_REGISTRY
+call:BANNER
+if exist "%~dp0psexec.exe" (
+	>nul 2>&1 reg add "HKLM\%AppCompatFlagsLayers%" /v "%AppDir%\ViPER4WindowsCtrlPanel.exe" /t REG_SZ /d "RUNASADMIN" /f
+	>nul 2>&1 reg add "HKCU\%AppCompatFlagsLayers%" /v "%AppDir%\ViPER4WindowsCtrlPanel.exe" /t REG_SZ /d "RUNASADMIN" /f
+	for %%a in (HKLM\SOFTWARE\Classes HKCR) do (
+		>nul 2>&1 .\psexec.exe -i -d -s -accepteula -nobanner reg delete "%%a\AudioEngine\AudioProcessingObjects" /f
+		>nul 2>&1 reg add "%%a\%APOSubPath%" /v "FriendlyName" /t REG_SZ /d "ViPER4Windows" /f
+		>nul 2>&1 reg add "%%a\%APOSubPath%" /v "Copyright" /t REG_SZ /d "Copyright (C) 2013, vipercn.com" /f
+		>nul 2>&1 reg add "%%a\%APOSubPath%" /v "MajorVersion" /t REG_DWORD /d "1" /f
+		>nul 2>&1 reg add "%%a\%APOSubPath%" /v "MinorVersion" /t REG_DWORD /d "0" /f
+		>nul 2>&1 reg add "%%a\%APOSubPath%" /v "Flags" /t REG_DWORD /d "13" /f
+		>nul 2>&1 reg add "%%a\%APOSubPath%" /v "MinInputConnections" /t REG_DWORD /d "1" /f
+		>nul 2>&1 reg add "%%a\%APOSubPath%" /v "MaxInputConnections" /t REG_DWORD /d "1" /f
+		>nul 2>&1 reg add "%%a\%APOSubPath%" /v "MinOutputConnections" /t REG_DWORD /d "1" /f
+		>nul 2>&1 reg add "%%a\%APOSubPath%" /v "MaxOutputConnections" /t REG_DWORD /d "1" /f
+		>nul 2>&1 reg add "%%a\%APOSubPath%" /v "MaxInstances" /t REG_DWORD /d "4294967295" /f
+		>nul 2>&1 reg add "%%a\%APOSubPath%" /v "NumAPOInterfaces" /t REG_DWORD /d "1" /f
+		>nul 2>&1 reg add "%%a\%APOSubPath%" /v "APOInterface0" /t REG_SZ /d "{FD7F2B29-24D0-4B5C-B177-592C39F9CA10}" /f
+	)
+	if %ERRORLEVEL% NEQ 0 (
+		call:BANNER
+		echo [失败!] 注册表修复失败.
+		>nul 2>&1 timeout /t 2
+		goto:CHOICE_MENU
+	) else (
+		call:BANNER
+		echo [提示] 注册表修复成功.
+	)
+) else (
+	mode con: cols=86 lines=20
 	call:BANNER
+	echo [警告!] psexec.exe 不存在，这可能会导致修复程序在 Windows 11 中失效
+	echo [警告!] 请前往 https://docs.microsoft.com/en-us/sysinternals/downloads/psexec 下载
+	echo [警告!] 然后将 psexec.exe 放置于修复程序同目录中，然后重新打开修复程序
+	echo [警告!] 10秒后尝试旧版修复方法
+	>nul 2>&1 timeout /t 10
 	for %%a in (HKLM\SOFTWARE\Classes HKCR) do (
 		>nul 2>&1 reg delete "%%a\AudioEngine\AudioProcessingObjects" /f
 		>nul 2>&1 reg add "%%a\AudioEngine\AudioProcessingObjects\{DA2FB532-3014-4B93-AD05-21B2C620F9C2}" /v "FriendlyName" /t REG_SZ /d "ViPER4Windows" /f
@@ -68,28 +113,37 @@ if "%CMVAR%"=="1" (
 		>nul 2>&1 reg add "%%a\AudioEngine\AudioProcessingObjects\{DA2FB532-3014-4B93-AD05-21B2C620F9C2}" /v "APOInterface0" /t REG_SZ /d "{FD7F2B29-24D0-4B5C-B177-592C39F9CA10}" /f
 	)
 	>nul 2>&1 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" /v "%APPDIR%\ViPER4WindowsCtrlPanel.exe" /t REG_SZ /d "RUNASADMIN" /f
-	echo [提示] 注册表修复成功！
-	>nul 2>&1 timeout /t 2
-	goto:CHOICE_MENU
-)
-if "%CMVAR%"=="2" (
-	if exist "%APPDIR%\Configurator.exe" (
-		start "" "%APPDIR%\Configurator.exe"
+	mode con: cols=56 lines=20
+	if %ERRORLEVEL% NEQ 0 (
+		call:BANNER
+		echo [失败!] 注册表修复失败.
+		>nul 2>&1 timeout /t 2
 		goto:CHOICE_MENU
 	) else (
 		call:BANNER
-		echo [失败!] ViPER4Windows 配置程序未找到.
-		>nul 2>&1 timeout /t 2
+		echo [提示] 注册表尝试修复成功！
+		echo [提示] 由于 'psexec.exe' 丢失，请手动确认修复是否成功！
+		>nul 2>&1 timeout /t 5
 		goto:CHOICE_MENU
 	)
 )
-if "%CMVAR%"=="3" (
+
+:LAUNCH_CONFIGURATOR
+if exist "%AppDir%\Configurator.exe" (
+	start "" "%AppDir%\Configurator.exe"
+	goto:CHOICE_MENU
+) else (
 	call:BANNER
-	powershell -command "Restart-Service -Name Audiosrv -Confirm:$false"
-	echo [完成] 音频服务已重启
+	echo [失败!] ViPER4Windows 配置程序未找到.
 	>nul 2>&1 timeout /t 2
 	goto:CHOICE_MENU
 )
+
+:RESTART_AUDIO_SERVICE
+powershell -command "Restart-Service -Name Audiosrv -Confirm:$false -Force"
+echo [完成] 音频服务已重启
+>nul 2>&1 timeout /t 2
+goto:CHOICE_MENU
 
 :BANNER
 cls                                   
